af3d22e3f10f899b8b01decfb98282861b48d97c
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2021-01-05 15:43:51 +0530

    Update 6. varhoisting.js

--- 6. varhoisting.js
+++ 6. varhoisting.js
@@ -1,12 +1,14 @@
-// Program to understand how variable hoisting works.
-
+// Programs to understand how variable hoisting works.
+-----------------------------------------------------------
+Program-1
+----------
 console.log(i)            // Output will be undefined as variable hoisting allows the use of variable without declaration.
 for (var i = 1;i <= 5;i++){
     console.log(i)
 }
 console.log("after the loop i value is ",i)
 
-Output
+Output-1
 -------
 undefined
 1
@@ -15,3 +17,15 @@ undefined
 4
 5
 after the loop i value is  6
+------------------------------------------------------------
+Program- 2
+-----------
+var balance = 5000
+console.log(typeof balance)
+var balance = {message:"hello"}
+console.log(typeof balance)
+
+Output- 2
+----------
+number
+object
